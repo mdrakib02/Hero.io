bdaffd74cc3dbc207fd29b6f5c3324399938f76d
mdrakib02z <mdrakibahmed515@gmail.comm>
2025-10-29 02:56:09 -0400

    Errpr Page Implement done

--- src/Error/ErrorPage.jsx
+++ src/Error/ErrorPage.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import bg404Page from "../assets/error-404.png"
+import { Link } from 'react-router'
 
 export default function ErrorPage() {
   return (
-    <div>ErrorPage</div>
+    <div className=''>
+            <div className='container mx-auto py-10 flex flex-col items-center justify-center'>
+
+                <div className='flex flex-col items-center gap-y-6 justify-between'>
+                    <img src={bg404Page} alt="" />
+                    <h4 className='text-4xl text-indigo-600 font-bold'>Oops, page not found!</h4>
+                    <Link to="/" className='btn bg-indigo-500 text-white'>Go Back!</Link> 
+                </div>
+
+            </div>
+
+        </div>
   )
 }
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -14,6 +14,7 @@ export const router = createBrowserRouter([
   {
     path: "/",
     Component: Root,
+    errorElement: <ErrorPage></ErrorPage>,
 
     children: [
       {
